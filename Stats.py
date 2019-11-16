69cc4026ff5ce4afb87984743d15b8b635db22f4
MelanieBrett <44926797+MelanieBrett@users.noreply.github.com>
2019-11-16 22:10:07 +0000

    Add files via upload

---
+++ Stats.py
@@ -0,0 +1,19 @@
+Stats = {'Money':5,
+'Environmental Awareness':5,
+'Environment':5,
+'Personal influence':3,
+'Pollution':5,
+'Global temperature change':0,
+'Sea level change':0,
+'Job level':1
+}
+
+Relationships = {'Activists':5,
+'Local townsperson':5,
+'Principal':5,
+'Right wing MP':5,
+'Left wing MP':5,
+}
+
+Time = 0
+
